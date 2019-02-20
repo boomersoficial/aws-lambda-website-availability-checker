15c2981e9a9a71ac12f82c09de1b7e1ffb02d0a1
will2dev <wylliam.bastiani@ame-sp.org.br>
2019-02-19 23:08:47 -0300

    refatorando

--- index.js
+++ index.js
@@ -3,32 +3,31 @@
 const ping = require('./ping');
 const sendMetric = require('./send_metrics');
 
+function validateProperties (event) {
+  if (!event) 
+    throw new Error('[AwsLambda-Website-Availability-Checker] Data not provided');
+
+  if (!event.url )
+    throw new Error('[AwsLambda-Website-Availability-Checker] Url not provided');
+
+  if (!event.metricNamespace)
+    throw new Error('[AwsLambda-Website-Availability-Checker] MetricNamespace not provided');
+
+  if (!event.metricName)
+    throw new Error('[AwsLambda-Website-Availability-Checker] MetricName not provided');
+}
+
 exports.handler = async (event) => {
-  
-  if (!event) {
-    console.log('[AwsLambda-Website-Availability-Checker] Data not provided');
-    return;
-  }
 
-  if (!event.url ) {
-    console.log('[AwsLambda-Website-Availability-Checker] Url not provided');
-    return;
-  }
+  try {
+    validateProperties(event);
 
-  if (!event.metricNamespace) {
-    console.log('[AwsLambda-Website-Availability-Checker] MetricNamespace not provided');
-    return;
-  }
+    const isWebsiteUpAndRunning = await ping(event.url);
+    await sendMetric(event.url, event.metricNamespace, event.metricName, isWebsiteUpAndRunning);
 
-  if (!event.metricName) {
-    console.log('[AwsLambda-Website-Availability-Checker] MetricName not provided');
-    return;
-  }
-
-  const isWebsiteUpAndRunning = await ping(event.url);
-  await sendMetric(event.url, event.metricNamespace, event.metricName, isWebsiteUpAndRunning)
-
-  console.log('isWebsiteUpAndRunning: ', isWebsiteUpAndRunning);
-
+    console.log(`Is website up: ${isWebsiteUpAndRunning}`);
+  } catch (ex) {
+    console.log(ex);
+  } 
 };
 
